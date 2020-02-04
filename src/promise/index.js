f42132abbb69da0621866f78dc4cae1337905f66
Victor Landa <victorlanda.dev@gmail.com>
2020-02-04 17:55:21 -0600

    promises

---
+++ src/promise/index.js
@@ -0,0 +1,36 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if(true) {
+      resolve('Success!');
+    } else {
+      reject('Error');
+    }
+  });
+};
+
+somethingWillHappen()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+somethingWillHappen2 = () => {
+  return new Promise((resolve, reject) => {
+    if(true) {
+      setTimeout(() => {
+        resolve('True');
+      }, 2000);
+    } else {
+      const error = new Error('False');
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappen2()
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+
+
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+  .then(response => console.log('Array of results', response)) // => Array of results [ 'Success!', 'True' ]
+  .catch(err => console.error(err));
